e30526efe1a611c630c69d1ea59d11ef134f08fa
Aaronnon <59756027+Aaronnon@users.noreply.github.com>
2020-12-24 03:39:55 -0500

    Stateless Component

--- my-app/src/TodoListUI.js
+++ my-app/src/TodoListUI.js
@@ -2,38 +2,42 @@ import React from "react";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import { Input, Button, List } from "antd";
 
-class TodoListUI extends React.Component {
-  render() {
-    return (
-      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
-        <div>
-          <Input
-            value={this.props.inputValue}
-            placeholder="Basic usage"
-            style={{ width: "300px", marginRight: "10px" }}
-            onChange={this.props.handleInputChange}
-          />
-          <Button type="primary" onClick={this.props.handleBtnClick}>
-            Submit
-          </Button>
-        </div>
-        <List
-          style={{ marginTop: "10px", width: "300px" }}
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (
-            <List.Item
-              onClick={(index) => {
-                this.props.handleItemDelete(index);
-              }}
-            >
-              {item}
-            </List.Item>
-          )}
+const TodoListUI = (props) => {
+  return (
+    <div style={{ marginTop: "10px", marginLeft: "10px" }}>
+      <div>
+        <Input
+          value={props.inputValue}
+          placeholder="Basic usage"
+          style={{ width: "300px", marginRight: "10px" }}
+          onChange={props.handleInputChange}
         />
+        <Button type="primary" onClick={props.handleBtnClick}>
+          Submit
+        </Button>
       </div>
-    );
-  }
-}
+      <List
+        style={{ marginTop: "10px", width: "300px" }}
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (
+          <List.Item
+            onClick={(index) => {
+              props.handleItemDelete(index);
+            }}
+          >
+            {item}
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
+
+// class TodoListUI extends React.Component {
+//   render() {
+
+//   }
+// }
 
 export default TodoListUI;
