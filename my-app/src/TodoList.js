0d089a80898685b88d24aeea7d6152e0e634131d
Aaronnon <59756027+Aaronnon@users.noreply.github.com>
2020-12-24 01:29:07 -0500

    state

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,32 +1,35 @@
+import React from "react";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import { Input, Button, List } from "antd";
+import store from "./store/index.js";
+// import { Component } from "react";
 
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires.",
-];
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+  }
 
-function TodoList() {
-  return (
-    <div style={{ marginTop: "10px", marginLeft: "10px" }}>
-      <div>
-        <Input
-          placeholder="Basic usage"
-          style={{ width: "300px", marginRight: "10px" }}
+  render() {
+    return (
+      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
+        <div>
+          <Input
+            value={this.state.inputValue}
+            placeholder="Basic usage"
+            style={{ width: "300px", marginRight: "10px" }}
+          />
+          <Button type="primary">提交</Button>
+        </div>
+        <List
+          style={{ marginTop: "10px", width: "300px" }}
+          bordered
+          dataSource={this.state.list}
+          renderItem={(item) => <List.Item>{item}</List.Item>}
         />
-        <Button type="primary">提交</Button>
       </div>
-      <List
-        style={{ marginTop: "10px", width: "300px" }}
-        bordered
-        dataSource={data}
-        renderItem={(item) => <List.Item>{item}</List.Item>}
-      />
-    </div>
-  );
+    );
+  }
 }
 
 export default TodoList;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoList from "./TodoList";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <TodoList />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<TodoList />, document.getElementById("root"));
