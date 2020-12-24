088a3a8b9299f9cb033063dff6dc9069db06ece0
Aaronnon <59756027+Aaronnon@users.noreply.github.com>
2020-12-24 02:14:54 -0500

    TodoList

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -8,6 +8,10 @@ class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handleStoreChange);
   }
 
   render() {
@@ -18,17 +22,47 @@ class TodoList extends React.Component {
             value={this.state.inputValue}
             placeholder="Basic usage"
             style={{ width: "300px", marginRight: "10px" }}
+            onChange={this.handleInputChange}
           />
-          <Button type="primary">提交</Button>
+          <Button type="primary" onClick={this.handleBtnClick}>
+            Submit
+          </Button>
         </div>
         <List
           style={{ marginTop: "10px", width: "300px" }}
           bordered
           dataSource={this.state.list}
-          renderItem={(item) => <List.Item>{item}</List.Item>}
+          renderItem={(item, index) => (
+            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
+              {item}
+            </List.Item>
+          )}
         />
       </div>
     );
+  }
+  handleInputChange(e) {
+    const action = {
+      type: "change_input_value",
+      value: e.target.value,
+    };
+    store.dispatch(action);
+  }
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+  handleBtnClick() {
+    const action = {
+      type: "add_todo_item",
+    };
+    store.dispatch(action);
+  }
+  handleItemDelete(index) {
+    const action = {
+      type: "delete_todo_item",
+      index,
+    };
+    store.dispatch(action);
   }
 }
 
